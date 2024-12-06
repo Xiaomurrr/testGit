69f014a8dc842cfa68b14cc3206da12db987961c
Vyacheslav <vak152@tpu.ru>
2024-12-06 11:11:04 +0700

    ? / / / /

--- testGit/Source.cpp
+++ testGit/Source.cpp
@@ -9,6 +9,30 @@ int main() {
 	cout << "Ââåäèòå N: ";
 	cin >> N;
 	cout << "АХахпхвхпвыхаывах: ";
+	if (N & M) //также будет выводить true, тк числа ненулевые (false выведет, если все биты в числе 0)
+		cout << "N & M - true" << endl;
+	else
+		cout << "N & M - false" << endl;
+	// || and |
+	if (N || M) //выведет true, если одно число ненулевое
+		cout << "N || M - true" << endl;
+	else
+		cout << "N || M - false" << endl;
+	if (N | M)
+		cout << "N | M - true" << endl;
+	else
+		cout << "N | M - false" << endl;
+
+	// ~ and !
+	if (!N)
+		cout << "!N - true" << endl;
+?
+/??
+?
+?
+/
+
+/
 
 	return 0;
 
